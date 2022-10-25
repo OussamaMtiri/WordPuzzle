06b079a1743e3b9b73035ed95d4424dc8026bc35
Oussama Mtiri <mtirioussama@gmail.com>
2022-10-26 00:20:49 +0100

    UnitTest

--- WordPuzzleTests/SearchServiceTests.cs
+++ WordPuzzleTests/SearchServiceTests.cs
@@ -12,45 +12,55 @@ namespace WordPuzzleTests
 {
     public class SearchServiceTests
     {
-        [Test]
-        public void LoadText()
+        [TestCase(26880)]
+        public void LoadText_CalculateLenght_ReturnLenghtOfDocument(int lenthExpected)
         {
             //Arrange
             var options = GetAppSettingOptions();
-
             var filesInputOutput = new FilesInputOutput(options, new Argument());
 
             //Act
             var wordsArray = filesInputOutput.LoadText();
 
             //Assert
-            Assert.AreEqual(26880, wordsArray.Length);
+            Assert.AreEqual(lenthExpected, wordsArray.Length);
         }
 
-        [Test]
-        public void GetWordsList()
+        [TestCase("spin", "spot", "res", 3)]
+        public void GetWordsList_Count_ReturnWordsCount(string startWord, string endWord, string resultFile, int lenthExpected)
         {
             //Arrange
             var options = GetAppSettingOptions();
-            Argument argument = new() { StartWord = "spin", EndWord = "spot", ResultFile = "res" };
+            Argument argument = new() { StartWord = startWord, EndWord = endWord, ResultFile = resultFile };
             LoadTextGetWordsService loadTextGetWordsService = new(new FilesInputOutput(options, argument));
 
             //Act
             var Array = loadTextGetWordsService.LoadTextAndGetWordsList(argument);
 
             //Assert
-            Assert.AreEqual(3, Array.Length);
+            Assert.AreEqual(lenthExpected, Array.Length);
         }
 
+        [TestCase("spin", "spot", null)]
+        public void UserArgumentsValidation_TryValidateNullArgument_ValidationFail(string startWord, string endWord, string resultFile)
+        {
+            //Arrange
+            Argument argument = new() { StartWord = startWord, EndWord = endWord, ResultFile = resultFile };
+            UserInputServices userInputServices = new(argument);
 
+            //Act
+            bool isValid = userInputServices.UserArgumentsValidation(argument);
 
+            //Assert
+            Assert.AreNotEqual(true, isValid);
+        }
 
         public static IOptions<MySettings> GetAppSettingOptions()
         {
             string appsettingsJson = File.ReadAllText($"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName}\\appsettingsTest.json");
             var mySetting = JsonSerializer.Deserialize<MySettings>(appsettingsJson);
             return Options.Create(mySetting);
-            
+
         }
     }
 }
